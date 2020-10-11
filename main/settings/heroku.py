e5967a17eb050677aed2152b6931cdf94a88be46
dannicada <dannynwangwu@gmail.com>
2020-10-11 10:51:39 +0100

    database fix: heroku

--- main/settings/heroku.py
+++ main/settings/heroku.py
@@ -98,7 +98,7 @@ DATABASES = {
 }
 
 prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+DATABASES['default'](prod_db)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
