581660279cd67be2811e717b2a7468678a83ec98
dannicada <dannynwangwu@gmail.com>
2020-10-11 15:17:20 +0100

    core api documentation

--- main/urls.py
+++ main/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
+# from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
+
 
 schema_view = get_swagger_view(title='Login API')
 
 
 urlpatterns = [
-    path(r'', schema_view),
+    # path(r'', schema_view),
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('docs/', include_docs_urls(title='BCC Backend-API', public=False))
 ]
