eb6900f8646052e47684f8e143cab54d77b19db6
dannicada <dannynwangwu@gmail.com>
2020-10-15 16:01:21 +0100

    events model

---
+++ events/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+from accounts.models import User
+
+
+class Event(models.Model):
+
+    name = models.CharField(max_length=500, blank=False, null=False)
+    venue = models.CharField(max_length=500, blank=True, null=True)
+    time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    capacity = models.BigIntegerField()
+    attendees = models.ManyToManyField(User, related_name='courses_joined', blank=True)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+   
+
+    class Meta: 
+        # Add verbose name 
+        # verbose_name = 'Main Section'
+        # verbose_name_plural = 'Main Section'
+        ordering = ['-created']
+
+
+# class Attendance(models.Model):
+
+#     event = models.OneToOneField(Event, on_delete=models.CASCADE, blank=False)
+#     attendees = models.ManyToManyField
