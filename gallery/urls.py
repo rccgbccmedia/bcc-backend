b58fbafdbdec92ed0e9854f5a4f03e3b0d1930d6
dannicada <dannynwangwu@gmail.com>
2020-11-15 12:11:41 +0100

    added tests for videos

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,12 +7,12 @@ from rest_framework.routers import DefaultRouter
 urlpatterns = [
     path('images/all/', views.ImageView.as_view({'get': 'list'}), name='all-images'),
     path('images/add/', views.ImageView.as_view({'post': 'create'}), name='add-image'),
-    path('images/<int:pk>/',views.ImageView.as_view({'get': 'retrieve'}), name='get-video'),
-    path('images/delete/<int:pk>/',views.ImageView.as_view({'get': 'destroy'}), name='delete-video'),
+    path('images/<int:pk>/',views.ImageView.as_view({'get': 'retrieve'}), name='get-image'),
+    path('images/delete/<int:pk>/',views.ImageView.as_view({'get': 'destroy'}), name='delete-image'),
 
 
-    path('video/all/', views.ImageView.as_view({'get': 'list'}), name='all-video'),
-    path('video/add/', views.ImageView.as_view({'post': 'create'}), name='add-image'),
-    path('video/<int:pk>/',views.ImageView.as_view({'get': 'retrieve'}), name='get-video'),
-    path('video/delete/<int:pk>/',views.ImageView.as_view({'get': 'destroy'}), name='delete-video'),
+    path('videos/all/', views.ImageView.as_view({'get': 'list'}), name='all-video'),
+    path('videos/add/', views.ImageView.as_view({'post': 'create'}), name='add-image'),
+    path('videos/<int:pk>/',views.ImageView.as_view({'get': 'retrieve'}), name='get-video'),
+    path('videos/delete/<int:pk>/',views.ImageView.as_view({'get': 'destroy'}), name='delete-video'),
 ]
