926022e7d7d1d43c646cda1ae794a2793f7f5cd7
dannicada <dannynwangwu@gmail.com>
2020-11-17 10:23:01 +0100

    fixed views for video url

--- gallery/urls.py
+++ gallery/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('images/delete/<int:pk>/',views.ImageView.as_view({'get': 'destroy'}), name='delete-image'),
 
 
-    path('videos/all/', views.ImageView.as_view({'get': 'list'}), name='all-video'),
-    path('videos/add/', views.ImageView.as_view({'post': 'create'}), name='add-image'),
-    path('videos/<int:pk>/',views.ImageView.as_view({'get': 'retrieve'}), name='get-video'),
-    path('videos/delete/<int:pk>/',views.ImageView.as_view({'get': 'destroy'}), name='delete-video'),
+    path('videos/all/', views.VideoView.as_view({'get': 'list'}), name='all-video'),
+    path('videos/add/', views.VideoView.as_view({'post': 'create'}), name='add-image'),
+    path('videos/<int:pk>/',views.VideoView.as_view({'get': 'retrieve'}), name='get-video'),
+    path('videos/delete/<int:pk>/',views.VideoView.as_view({'get': 'destroy'}), name='delete-video'),
 ]
